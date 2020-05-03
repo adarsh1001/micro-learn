7d35eb64fd0fa87347d46f0100d35b6c8fd52ced
Adarsh Pal Singh <adarshpalsingh1996@gmail.com>
2020-05-03 20:41:47 +0530

    Support for LDA inference params calculation

--- microlearn/ml_models/LinearDiscriminantAnalysis.py
+++ microlearn/ml_models/LinearDiscriminantAnalysis.py
@@ -5,14 +5,33 @@ class OffloadLDA:
     def __init__(self, model):
         self.class0 = model.__dict__['classes_'][0]
         self.class1 = model.__dict__['classes_'][1]
-        self.cov_matrix = model.covariance_
+        try:
+            self.cov_matrix = model.covariance_
+        except AttributeError:
+            print("LDA must be trained with 'store_covariances=True' parameter.")
+            raise
+        self.inv_cov_matrix = np.linalg.inv(self.cov_matrix)
         self.mean_vector0 = model.means_[0]
         self.mean_vector1 = model.means_[1]
         self.prior0 = model.priors_[0]
         self.prior1 = model.priors_[1]
+        self.dim = len(self.mean_vector0)
+
+        self.w = self.get_weights()
+        self.c = self.get_intercept()
 
     def get_weights(self):
-        pass
+        return np.matmul(self.inv_cov_matrix, (self.mean_vector1 - self.mean_vector0))
 
     def get_intercept(self):
+        term1 = math.log(self.prior0/self.prior1)
+        term2 = 0.5*(np.matmul(np.matmul(self.mean_vector0, self.inv_cov_matrix), self.mean_vector0) - np.matmul(np.matmul(self.mean_vector1, self.inv_cov_matrix), self.mean_vector1))
+        return term1 - term2
+    
+    def get_params(self):
+        return {'Weight_Vector':self.w, 'Intercept_Constant':self.c}
+
+    def get_arduino_code(self):
         pass
+
+    
--- microlearn/offloader.py
+++ microlearn/offloader.py
@@ -1,11 +1,13 @@
-from ml_models.LinearDiscriminantAnalysis import OffloadLDA
+from .ml_models.LinearDiscriminantAnalysis import OffloadLDA
 
 class Offload:
     supported_algorithms = ["LinearDiscriminantAnalysis", "QuadraticDiscriminantAnalysis", "GaussianNB", "SVC"]
 
-    def __init__(self):
-        self.offloader = None
-        self.algorithm = None
+    def __init__(self, model):
+        self.check_model_validity(model)
+        self.model = model
+        self.algorithm = self.get_algorithm(model)
+        self.offloader = self.get_offloader()
 
     def get_algorithm(self, model):
         return model.__repr__().split('(')[0]
@@ -25,19 +27,31 @@ class Offload:
             model.__dict__["classes_"]
         except KeyError:
             return False
-        if len(model.__dict__["classes_"]) != 2:
-            return False
         return True
 
-    def offload(self, model):
+    def is_model_binary(self, model):
+        return len(model.__dict__["classes_"]) == 2
+
+    def get_offloader(self):
+        if self.algorithm == self.supported_algorithms[0]:  #LDA
+            return OffloadLDA(self.model)     
+    
+    def check_model_validity(self, model):
         if self.is_algorithm_supported(model):
             self.algorithm = self.get_algorithm(model)
         else:
             raise TypeError("Input ML model not supported! Only LDA, QDA, GNB and SVM of scikit-learn are supported.")
 
         if not self.is_model_trained(model):
-            raise TypeError("Input ML model not trained on a binary-class dataset! First .fit() on a dataset and then offload.")
+            raise TypeError("Input ML model not trained on a dataset! First .fit() on a dataset and then offload.")
 
-        
+        if not self.is_model_binary(model):
+            raise TypeError("Input ML model trained on a multiclass dataset! Only binary-class models are supported.")
+
+    def get_params(self):
+        return self.offloader.get_params()
+
+    def export_to_arduino(self, model):
+        pass
 
 
