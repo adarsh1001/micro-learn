2a8ac56c57cbf1a8dbf14f27de999ba59139a3d2
Adarsh Pal Singh <adarshpalsingh1996@gmail.com>
2020-06-05 23:24:15 +0530

    Makes spaces consistant with others

--- microlearn/ml_models/LogisticRegression.py
+++ microlearn/ml_models/LogisticRegression.py
@@ -1,5 +1,3 @@
-
-
 import math
 import numpy as np 
 
@@ -14,31 +12,37 @@ class OffloadLogisticRegression:
     def get_params(self):
         return {'Weight_Vector':self.w, 'Bias_Constant':self.b}
 
-
     def get_arduino_code(self):
         str_w = ', '.join([str(x) for x in self.w])
 
-        code = f"""
-        #include <math.h>
-        double w[] = {{{str_w}}};
+        code = f"""#include <math.h>
+        
+double w[] = {{{str_w}}};
 double b = {str(self.b)};
+
+double sigmoid(double x)
+{{
+    return (1 / (1 + exp((-1) * x)));        
+}}
+
 void setup() {{
     Serial.begin(9600);
+
 }}
+
 void loop() {{
     //Data Section: To Be Coded Manually
-    float data[{str(self.dim)}]; //This is your feature vector. Retrive your data into this array.
-    //ML Inference Section
-    double temp = 0.0;
 
-    double sigmoid(double x)
-    {{
-        return (1 / (1 + exp((-1) * x)));        
-    }}
+    float data[{str(self.dim)}]; //This is your feature vector. Retrive your data into this array.
+
+    //ML Inference Section
+
+    double temp = 0.0;
     for(int i=0; i<{str(self.dim)}; i++)
     {{
         temp += data[i]*w[i];
     }}
+
     if(round(sigmoid(temp + b)) == 1)
     {{
         //Do something for class label {str(self.class1)}.
